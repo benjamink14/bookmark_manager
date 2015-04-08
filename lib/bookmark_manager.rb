5dae737cd46e5a16ace0be0d23686319aadfbfde
Benjamin Kollek <benkollek@gmail.com>
2015-04-08 16:37:38 +0100

    No redirection to homepage when passwords do not match upon sign up

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -1,11 +1,14 @@
 require 'sinatra/base'
 require_relative 'database_setup'
+require 'rack-flash'
 
 class BookmarkManager < Sinatra::Base
   set :views, Proc.new { File.join(root,"..", "views")}
 
   enable :sessions
   set :session_secret, 'super secret'
+
+  use Rack::Flash
 
   get '/' do
     @links = Link.all
@@ -30,14 +33,20 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email], password: params[:password],
+    @user = User.create(email: params[:email], password: params[:password],
                        password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash[:notice] = 'Sorry, your passwords do not match'
+      erb :'users/new'
+    end
   end
 
   helpers do
